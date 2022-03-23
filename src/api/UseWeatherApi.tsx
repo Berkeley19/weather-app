779f6bcf43a4dfd7fb88a20a70dfda2614dc1224
Berkeley V-H <berkeleyvh20@gmail.com>
2022-03-23 23:24:59 +0000

    added useState

--- src/api/UseWeatherApi.tsx
+++ src/api/UseWeatherApi.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { WeatherDisplayData, WeatherResponseData, WindResponseData, TempResponseData } from '../utils/interfaces'
+import { WeatherDisplayData, WeatherResponseData, WindResponseData, TempResponseData, FinalWeatherData } from '../utils/interfaces'
 
 export const useWeatherApi = ({lat,lng,openSideState: [openSide, setOpenSide]}: WeatherDisplayData) => {
+    const [finalData, setFinalData] = React.useState<FinalWeatherData>();
 
     React.useEffect(() => {
         if(openSide){
@@ -16,14 +17,12 @@ export const useWeatherApi = ({lat,lng,openSideState: [openSide, setOpenSide]}: 
               };
           (async () => {
             let {...response} = await fetchData()
-            console.log(response)
-            let weatherResponse: WeatherResponseData = response.weather[0]
-            let tempResponse: TempResponseData = { feels_like: response.main.feels_like, temp: response.main.temp }
-            let windResponse: WindResponseData = { wind_speed: response.wind.speed }
-            console.log(windResponse)
-            console.log(weatherResponse)
-            console.log(tempResponse)
-            
+            const weatherResponse: WeatherResponseData = response.weather[0]
+            const tempResponse: TempResponseData = { feels_like: response.main.feels_like, temp: response.main.temp }
+            const windResponse: WindResponseData = { wind_speed: response.wind.speed }
+            setFinalData({...weatherResponse, ...tempResponse, ...windResponse})
+            // setFinalData({...weatherResponse, ...tempResponse, ...windResponse})            
+            console.log(finalData)
           })()
         }
        },
